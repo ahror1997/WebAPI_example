18af53a87a1db47f879150815d6f95036f048cea
Ahror Ziyodinov <ziyodinov.ahror@loc.tashkent.uz>
2022-05-07 14:19:25 +0500

    Add project files.

---
+++ WebAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly DataContext _context;
+        
+        public SuperHeroController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> Index()
+        {
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> Show(int id)
+        {
+            var hero = await _context.SuperHeroes.FindAsync(id);
+            if (hero == null)
+            {
+                return NotFound("Hero not found!");
+            }
+            return Ok(hero);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<SuperHero>> Create(SuperHero hero)
+        {
+            _context.SuperHeroes.Add(hero);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<SuperHero>> Update(SuperHero request)
+        {
+            var hero = await _context.SuperHeroes.FindAsync(request.Id);
+            if (hero == null)
+            {
+                return BadRequest("Hero not found!");
+            }
+            hero.Nickname = request.Nickname;
+            hero.FirstName = request.FirstName;
+            hero.LastName = request.LastName;
+            hero.IsAlive = request.IsAlive;
+            
+            await _context.SaveChangesAsync();
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<SuperHero>> Delete(int id)
+        {
+            var hero = await _context.SuperHeroes.FindAsync(id);
+            if (hero == null)
+            {
+                return BadRequest("Hero not found!");
+            }
+            _context.SuperHeroes.Remove(hero);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+    }
+}
---
+++ WebAPI/SuperHero.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        public string Nickname { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public bool IsAlive { get; set; } = false;
+    }
+}
